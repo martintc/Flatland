6a769c0dd766fcc4577bc1a0bc84a6ddff62d427
Todd Martin <kj4ntv@gmail.com>
2024-05-27 21:04:15 -0700

    Make a public initializer for InstancePayload

--- Sources/Flatland/Types/InstancePayload.swift
+++ Sources/Flatland/Types/InstancePayload.swift
@@ -11,4 +11,9 @@ import simd
 public struct InstancePayload {
     public var model: simd_float4x4
     public var textureCoordinates: simd_float4x2
+
+    public init(model: simd_float4x4, textureCoordinates: simd_float4x2) {
+        self.model = model
+        self.textureCoordinates = textureCoordinates
+    }
 }
