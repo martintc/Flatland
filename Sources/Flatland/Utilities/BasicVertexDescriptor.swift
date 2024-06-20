32fcdd0b2e7978954833c74eee4cd55ea1687c0b
Todd Martin <kj4ntv@gmail.com>
2024-06-19 20:34:33 -0700

    Convert to using a getter property

--- Sources/Flatland/Utilities/BasicVertexDescriptor.swift
+++ Sources/Flatland/Utilities/BasicVertexDescriptor.swift
@@ -10,20 +10,20 @@ import simd
 
 public class BasicVertexDescriptor {
 
-    public var vertexDescriptor: MTLVertexDescriptor
+    public var vertexDescriptor: MTLVertexDescriptor {
+        get {
+            let mvd = MTLVertexDescriptor()
 
-    public init() {
-        let mvd = MTLVertexDescriptor()
+            mvd.layouts[0].stepFunction = .perVertex
+            mvd.layouts[0].stepRate = 1
+            mvd.layouts[0].stride = MemoryLayout<simd_float3>.stride
 
-        mvd.layouts[0].stepFunction = .perVertex
-        mvd.layouts[0].stepRate = 1
-        mvd.layouts[0].stride = MemoryLayout<simd_float3>.stride
-        
-        mvd.attributes[0].format = .float3
-        mvd.attributes[0].bufferIndex = 0
-        mvd.attributes[0].offset = 0
-
-        self.vertexDescriptor = mvd
+            mvd.attributes[0].format = .float3
+            mvd.attributes[0].bufferIndex = 0
+            mvd.attributes[0].offset = 0
+            
+            return mvd
+        }
     }
     
 }
