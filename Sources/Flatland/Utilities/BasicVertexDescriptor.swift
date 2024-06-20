67745e1d7d92edbfba6f5875f401e056d9f24d78
Todd Martin <kj4ntv@gmail.com>
2024-06-19 20:25:06 -0700

    Add a basic vertex descriptor

---
+++ Sources/Flatland/Utilities/BasicVertexDescriptor.swift
@@ -0,0 +1,29 @@
+//
+//  BasicVertexDescriptor
+//  Flatland
+//
+//  Created by Todd Martin 6/10/24
+//
+
+import Metal
+import simd
+
+public class BasicVertexDescriptor {
+
+    public var vertexDescriptor: MTLVertexDescriptor
+
+    init() {
+        let mvd = MTLVertexDescriptor()
+
+        mvd.layouts[0].stepFunction = .perVertex
+        mvd.layouts[0].stepRate = 1
+        mvd.layouts[0].stride = MemoryLayout<simd_float3>.stride
+        
+        mvd.attributes[0].format = .float3
+        mvd.attributes[0].bufferIndex = 0
+        mvd.attributes[0].offset = 0
+
+        self.vertexDescriptor = mvd
+    }
+    
+}
