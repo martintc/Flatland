d1472685687b98865688f8fbca60748761d4789d
Todd Martin <kj4ntv@gmail.com>
2024-07-10 15:26:01 -0700

    Add a singleton that wraps MTLLibrary for ease of use and reference

---
+++ Sources/Flatland/Libraries/MetalLibrary.swift
@@ -0,0 +1,33 @@
+//
+//  MetalLibrary.swift
+//  Flatland
+//
+// Created by Todd Martin on 7/10/24
+//
+
+import Metal
+
+public class MetalLibrary {
+    static let shared = MetalLibrary()
+
+    private var library: MTLLibrary? = nil
+    private var functions: [String: any MTLFunction] = [String: any MTLFunction]()
+
+    /// Create a library
+    /// - Parameter device: MTLDevice to create a default library from
+    public func createLibrary(device: MTLDevice) {
+        library = device.makeDefaultLibrary()
+    }
+
+    /// Make a function by name and add it to the library
+    /// - Parameter name: Name of the function to load
+    public func makeFunction(name: String) {
+        functions[name] = library?.makeFunction(name: name)
+    }
+
+    /// Get a function from the library if present
+    /// - Parameter name: Name of the function to get
+    public func getFunction(name: String) -> (any MTLFunction)? {
+        functions[name]
+    }
+}
